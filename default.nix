924d4fec985dcac8db0f3b9aba5a76ada878a7c9
Dustin Lacewell <dlacewell@gmail.com>
2017-10-23 17:43:27 -0500

    Update for new plugin methodology

--- default.nix
+++ default.nix
@@ -1,37 +1,19 @@
-{ pkgs, lib }:
+{ config, pkgs, lib, ... }:
 
-with lib;
-with builtins;
+{
+  config.nixpkgs.overlays = [(self: super: {
 
+    org-build = { source }: let
+      # the build uses emacs to perform the conversion
+      env = { buildInputs = [ pkgs.emacs ]; };
+      # call make-init-el to convert the file
+      script = { source }: ''
+        ln -s "${source}" ./init.org;
+        emacs -Q --script "${./org-build.el}" -f make-init-el;
+        cp ./init.el $out;
+      '';
+    in pkgs.runCommand "org-build" env (script { inherit source; });
 
-rec {
-  build = { source }: let
-    # the build uses emacs to perform the conversion
-    env = { buildInputs = [ pkgs.emacs ]; };
-    # call make-init-el to convert the file
-    script = ''
-      ln -s "${source}" ./init.org;
-      emacs -Q --script "${./org-build.el}" -f make-init-el;
-      cp ./init.el $out;
-    '';
-  in pkgs.runCommand "org-build" env script;
-
-  module = {config, pkgs, lib, ...}:
-  let
-    cfg = config.plugins.org-build;
-  in {
-    options.plugins.org-build = {
-      enable = mkEnableOption "Build init.el from Orgmode file";
-      source = mkOption {
-        type = types.path;
-        description = ''
-          The source orgfile to build as init.el
-        '';
-      };
-    };
-
-    config = mkIf cfg.enable {
-      home.file.".emacs.d/init.el".source = build { source=cfg.source; };
-    };
-  };
+  })];
 }
+
